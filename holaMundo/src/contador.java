9cabe9c097f423c0dfee06a52e2341f8991809bb
Marcelo Rafael Denis Meza <mdrm016@gmail.com>
2014-03-16 22:57:25 -0300

    nuevos archivos

---
+++ holaMundo/src/contador.java
@@ -0,0 +1,8 @@
+
+public class contador {
+	private static int numero = 0;
+	
+	public int get_cuenta(){
+		return numero + 1;
+	}
+}
--- holaMundo/src/hola.java
+++ holaMundo/src/hola.java
@@ -3,6 +3,8 @@ public class hola {
 	public static void main (String [] args){
 		//comentario agregado
 		//hola
-		System.out.println("holaaaaaaa que tal mundo");
+		System.out.println("holaaaaaaa que tal mundo\n");
+		contador contar = new contador();
+		System.out.println(contar.get_cuenta() + "\n");
 	}
 }
