1d54fb4b581c2cd4923959afd8e18412dc538d40
Eduardo Gimenez <eduardogimenez8@gmail.com>
2014-04-06 16:36:57 -0400

    modificaciones a form de usuarios

--- sap/aplicaciones/usuarios/forms.py
+++ sap/aplicaciones/usuarios/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from .models import Usuarios
 
 class UsuarioNuevoForm(ModelForm):
     class Meta:
