a6c0149cadb469c7a9fec840985917e2d96fbaa6
ysapy <ysapy@Ysapy-Pc>
2014-04-06 14:52:41 -0400

    en view de usuario, nuevo crearusuario

--- sap/aplicaciones/usuarios/views.py
+++ sap/aplicaciones/usuarios/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+from forms import UsuarioNuevoForm
 
 # Create your views here.
+
+def UsuarioNuevo(request):
+    if request.method == 'POST':
+        form = UsuarioNuevoForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            email = form.cleaned_data["email"]
+            telefono = form.cleaned_data["telefono"] 
+            direccion = form.cleaned_data["direccion"]
+            especialidad = form.cleaned_data["especialidad"]
+            observaciones = form.cleaned_data["observaciones"]
+            
+            user = User.objects.create_user(username, email, password, telefono, direccion, especialidad, observaciones)
+            user.save()
+            
+            return HttpResponseRedirect(reverse('inicio'))
+        else:
+            form = UsuarioNuevoForm()
+            
+        data = {
+                'form': form,
+                }
+        return render_to_response('usuarionuevo.html', data, context_instance=ResquestContext(request))
