c6ef08fef4d45cd734f5840ce21bd5fe4ea2382f
Eduardo Gimenez <eduardogimenez8@gmail.com>
2014-04-06 15:05:03 -0400

    base_general.html prueba

--- sap/aplicaciones/usuarios/views.py
+++ sap/aplicaciones/usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Usuarios
 
 # Create your views here.
-#def Administrar_usuarios(request):
+def Administrar_usuarios(request):
 	#recibir la solicitud y listar los usuarios
-	
+	cusuarios = Usuarios.objects.all()
+	return render(request, 'usuarios.html', {'lista_usuarios': usuarios})
