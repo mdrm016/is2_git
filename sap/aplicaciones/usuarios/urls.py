548904bf7d447f442c2938f874bfe1cb1db2c51a
ysapy <ysapy@Ysapy-Pc>
2014-04-07 16:16:57 -0400

    mensaje de confirmacion de creacion de usuario, usuariocreado.html

--- sap/aplicaciones/usuarios/urls.py
+++ sap/aplicaciones/usuarios/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
 from .views import usuario_nuevo
 from .views import Administrar_usuarios
+from .views import usuarionuevo
 
 urlpatterns = patterns('',
         url(r'^adm_usuarios/nuevo/$', usuario_nuevo),
         url(r'^adm_usuarios/$', Administrar_usuarios),
+        url(r'^adm_usuarios/nuevo/enviar/$', usuarionuevo),
+
 )
