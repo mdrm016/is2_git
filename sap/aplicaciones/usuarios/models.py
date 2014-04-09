b1cad3d969538d70fb742a7842d3c512dfe6820f
ysapy <ysapy@Ysapy-Pc>
2014-04-08 22:28:37 -0400

    modificacion modelo User, creacion de usuario

--- sap/aplicaciones/usuarios/models.py
+++ sap/aplicaciones/usuarios/models.py
@@ -4,13 +4,18 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class Usuarios(models.Model):
+User.add_to_class('telefono', models.CharField(max_length=30))
+User.add_to_class('direccion', models.CharField(max_length=100))
+User.add_to_class('especialidad', models.CharField(max_length=50))
+User.add_to_class('observaciones', models.CharField(max_length=300))
+
+"""class Usuarios(models.Model):
     
     user = models.OneToOneField(User)
     telefono = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
     especialidad = models.CharField(max_length=50)
-    observaciones = models.TextField(max_length=300)
+    observaciones = models.TextField(max_length=300)"""
     
-    def __unicode__(self):
-        return self.user.username
+    #def __unicode__(self):
+     #   return self.user.username
--- sap/aplicaciones/usuarios/urls.py
+++ sap/aplicaciones/usuarios/urls.py
@@ -4,8 +4,8 @@ from .views import Administrar_usuarios
 from .views import usuarionuevo
 
 urlpatterns = patterns('',
-        url(r'^adm_usuarios/nuevo/$', usuario_nuevo),
+        url(r'^adm_usuarios/nuevo/$', usuarionuevo),
         url(r'^adm_usuarios/$', Administrar_usuarios),
-        url(r'^adm_usuarios/nuevo/enviar/$', usuarionuevo),
+        url(r'^adm_usuarios/nuevo/enviar/$', usuario_nuevo),
 
 )
