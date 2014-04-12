c0cebd4055d6ac474a60c4deced9c29649e02323
ysapy <ysapy@Ysapy-Pc>
2014-04-11 21:12:42 -0400

    user.json con todos los usuarios

--- sap/aplicaciones/usuarios/models.py
+++ sap/aplicaciones/usuarios/models.py
@@ -7,7 +7,11 @@ from django.db.models.signals import post_save
 
 class Usuarios(models.Model):
     """ La clase Usuarios crea un perfil a cada instancia de la clase
-         User, con los atributos descritos en este modelo 
+         User, con los atributos descritos en este modelo.
+         Contiene como atributo los campos: telefono, direccion,
+         especialidad, observaciones, id_user, este ultimo representa la 
+         clave foranea de la clase User, de esa manera para cada
+         User existe un unico perfil, y para cada perfil un unico User.
     
     @author: Ysapy Ortiz
     """  
--- sap/documentacion.sh
+++ sap/documentacion.sh
@@ -1,23 +1,23 @@
 #!/bin/bash 
 
-Usuario=`whoami`
+Usuario='ysapy'
 
-cd /home/"$Usuario"/is2_git/is2_git/sap/aplicaciones/autenticacion
-mkdir /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion
-mkdir /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion/Autenticacion
-epydoc --html --o /home/mdrm016/is2_git/is2_git/sap/Sap_documentacion/Autenticacion --name "Sap_documentacion" *.py
+cd /home/"$Usuario"/workspace/is2_git/sap/aplicaciones/autenticacion
+mkdir /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion
+mkdir /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion/Autenticacion
+epydoc --html --o /home/ysapy/workspace/is2_git/sap/Sap_documentacion/Autenticacion --name "Sap_documentacion" *.py
 
-cd /home/"$Usuario"/is2_git/is2_git/sap/aplicaciones/usuarios
-mkdir /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion/Usuarios
-epydoc --html --o /home/mdrm016/is2_git/is2_git/sap/Sap_documentacion/Usuarios --name "Sap_documentacion" *.py
+cd /home/"$Usuario"/workspace/is2_git/sap/aplicaciones/usuarios
+mkdir /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion/Usuarios
+epydoc --html --o /home/ysapy/workspace/is2_git/sap/Sap_documentacion/Usuarios --name "Sap_documentacion" *.py
 
-cd /home/"$Usuario"/is2_git/is2_git/sap
-mkdir /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion/Sap
-epydoc --html --o /home/mdrm016/is2_git/is2_git/sap/Sap_documentacion/Sap --name "Sap_documentacion" *.py
+cd /home/"$Usuario"/workspace/is2_git/sap
+mkdir /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion/Sap
+epydoc --html --o /home/ysapy/workspace/is2_git/sap/Sap_documentacion/Sap --name "Sap_documentacion" *.py
 
-cd /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion/Sap 
+cd /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion/Sap 
 sudo firefox index.html
-cd /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion/Usuarios
+cd /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion/Usuarios
 sudo firefox index.html
-cd /home/"$Usuario"/is2_git/is2_git/sap/Sap_documentacion/Autenticacion
+cd /home/"$Usuario"/workspace/is2_git/sap/Sap_documentacion/Autenticacion
 sudo firefox index.html
