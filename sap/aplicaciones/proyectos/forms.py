76de96e4eebd2c9f40c24ac866bcb134beb18277
mdrm016 <mdrm016@mdrm016>
2014-04-16 16:01:35 -0400

    Modificacion de proyecto realizado, falta comprobar con fases y comite para pasar de un estado inactivo a en construccion

--- sap/aplicaciones/proyectos/forms.py
+++ sap/aplicaciones/proyectos/forms.py
@@ -17,7 +17,14 @@ def validate_duracion_proyecto (value):
     if value < 1:
         raise ValidationError(u'La duracion debe ser mayor a cero.')
     
-opcionLider = [('', '--selectione un lider--')] + [(Usuario.id, Usuario.username) for Usuario in User.objects.all()] 
+opcionLider = [('', '--seleccione un lider--')] + [(Usuario.id, Usuario.username) for Usuario in User.objects.all()] 
+
+ESTADOS_PROYECTO=(
+        ('', '--seleccione un Estado--'),
+        ('Inactivo', 'Inactivo'),
+        ('En Construccion', 'En Construccion'),
+        ('Finalizado', 'Finalizado'),
+    )
   
 class ProyectoNuevoForm(forms.Form):
     
@@ -26,7 +33,12 @@ class ProyectoNuevoForm(forms.Form):
     Fecha_de_Inicio =  forms.DateField(input_formats=['%Y-%m-%d'], widget=widgets.AdminDateWidget,  required=True, help_text='* Ingrese en formato anho-mes-dia', error_messages={'required': 'Ingrese una fecha de inicio de proyecto'} )
     Duracion = forms.IntegerField(required=True, help_text='* En semanas', validators=[validate_duracion_proyecto], error_messages={'required': 'Ingrese la duracion del proyecto',})
     
-    def clean(self):
-        super(forms.Form, self).clean()
-        return self.cleaned_data
+        
+class ProyectoModificadoForm(forms.Form):
+    Nombre_del_Proyecto = forms.CharField(widget=forms.TextInput(), max_length=15, min_length=2, required=True, error_messages={'required': 'Ingrese un nombre para el proyecto', 'max_length': 'Longitud maxima: 15', 'min_length': 'Longitud minima: 2 caracteres'})
+    Lider_Actual = forms.CharField(widget=forms.TextInput(), required=False)
+    Nuevo_Lider =  forms.ChoiceField(widget=forms.Select(), choices= (opcionLider), required=False)
+    Estado_Actual = forms.CharField(widget=forms.TextInput(), required=False)
+    Nuevo_Estado = forms.ChoiceField(widget=forms.Select(), choices= (ESTADOS_PROYECTO), required=False)
+    Duracion = forms.IntegerField(required=True, help_text='En semanas', validators=[validate_duracion_proyecto], error_messages={'required': 'Ingrese la duracion del proyecto',})
     
--- sap/aplicaciones/proyectos/views.py
+++ sap/aplicaciones/proyectos/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from .forms import ProyectoNuevoForm
+from .forms import ProyectoNuevoForm, ProyectoModificadoForm
 from .models import Proyectos
 from django.contrib.auth.models import User
 
 def crear_proyecto (request):
     if request.method == 'POST':
         form = ProyectoNuevoForm(request.POST)
-        print form.errors
         if form.is_valid():
             form.clean()
             nombre = form.cleaned_data['Nombre_del_Proyecto'] 
@@ -33,5 +32,55 @@ def crear_proyecto (request):
         
     ctx ={'form': form}      
     template_name='proyectos/crearproyecto.html'
-    #return render_to_response(template_name, ctx, context_instance=RequestContext(request))
-    return render(request, template_name, {'form': form})
+    return render_to_response(template_name, ctx, context_instance=RequestContext(request))
+
+def modificar_proyecto (request, id_usuario):
+    proyecto = Proyectos.objects.get(id=id_usuario)
+    mensaje=''
+    if request.method == 'POST':
+        form = ProyectoModificadoForm(request.POST)
+        if form.is_valid():
+            form.clean()
+            nombreNuevo = form.cleaned_data['Nombre_del_Proyecto'] 
+            lider =  form.cleaned_data['Nuevo_Lider']
+            estado = form.cleaned_data['Nuevo_Estado']
+            duracion =  form.cleaned_data['Duracion']
+            
+            #Si no se ha suministrado un nuevo lider, el proyecto se queda con el lider actual
+            if not lider:
+                lider = User.objects.get(username=proyecto.lider)
+                lider = lider.id
+            lideruser = User.objects.get(id=lider)
+            
+            if not estado:
+                estado = proyecto.estado
+            
+            #si exite ya un proyecto con el nombre suministrado y el nombre suminitrado es distinto al del proyecto que esta siendo modificado
+            # Comprobar con fases y miembros de comite para pasar a un estado en construccion con un elif
+            if Proyectos.objects.filter(nombre=nombreNuevo) and nombreNuevo != proyecto.nombre:
+                data ={'Nombre_del_Proyecto':nombreNuevo, 'Lider_Actual':lider, 'Duracion':duracion}  
+                form = ProyectoModificadoForm(data)
+                mensaje = 'El nombre del proyecto ya existe y no puede haber duplicados'
+                
+            else:
+                if nombreNuevo == proyecto.nombre and  lideruser == proyecto.lider and estado == proyecto.estado and duracion == proyecto.duracion:
+                      mensaje="Proyecto Guardado sin modificaciones"
+                else:
+                    proyecto.nombre=nombreNuevo
+                    proyecto.lider=lideruser
+                    proyecto.estado = estado
+                    proyecto.duracion=duracion
+                    proyecto.save()
+                    mensaje="Proyecto modificado exitosamente"
+                    
+                ctx = {'mensaje':mensaje}
+                template_name='proyectos/proyectoalerta.html'
+                return render_to_response(template_name, ctx, context_instance=RequestContext(request))
+    else:
+        data ={'Nombre_del_Proyecto':proyecto.nombre, 'Lider_Actual':proyecto.lider, 'Estado_Actual':proyecto.estado, 'Duracion':proyecto.duracion}   
+        form = ProyectoModificadoForm(data)
+        
+    ctx ={'form': form, 'mensaje':mensaje}      
+    template_name='proyectos/modificarproyecto.html'
+    return render_to_response(template_name, ctx, context_instance=RequestContext(request))
+
