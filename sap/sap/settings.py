5dafe8ed29b8366346feeb7d474fc99e9f98c3fc
Eduardo Gimenez <eduardogimenez8@gmail.com>
2014-05-14 15:56:52 -0400

    arreglo en el archivo sap.wsgi.. (no  te preocupes marce) es solo un cambio que ya se habia hecho hace rato pero vos no tenias por que no hacias bien el pull y el push.. pero bueno... ahora ya sabes =D)

--- sap/sap/settings.py
+++ sap/sap/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'sap.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sap',
+        'NAME': 'sap_produccion',
         'USER': 'sap',
         'PASSWORD': 'sap',
         'HOST': '127.0.0.1',
