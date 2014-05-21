d7bc32faea15dcdb126854a957c6ffda57c32a88
ysapy <ysapy@Ysapy-Pc>
2014-05-20 20:51:49 -0400

    consultar solicitud, cancelar solicitud

--- sap/aplicaciones/solicitudes/urls.py
+++ sap/aplicaciones/solicitudes/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import crear_solicitud
+from .views import crear_solicitud, cancelar_solicitud, consultar_solicitud
 
 urlpatterns = patterns('',
-                       url(r'^adm_proyectos/gestionar/(?P<id_proyecto>\d+)/adm_items/(?P<id_fase>\d+)/solicitud/(?P<id_item>\d+)/$', crear_solicitud),
+                       url(r'^adm_proyectos/gestionar/(?P<id_proyecto>\d+)/adm_items/(?P<id_fase>\d+)/crear_solicitud/(?P<id_item>\d+)/$', crear_solicitud),
+                       url(r'^adm_proyectos/gestionar/(?P<id_proyecto>\d+)/adm_items/(?P<id_fase>\d+)/cancelar_solicitud/(?P<id_solicitud>\d+)/$', cancelar_solicitud),
+                       url(r'^adm_proyectos/gestionar/(?P<id_proyecto>\d+)/adm_items/(?P<id_fase>\d+)/consultar_solicitud/(?P<id_solicitud>\d+)/$', consultar_solicitud),
                        )
--- sap/aplicaciones/solicitudes/views.py
+++ sap/aplicaciones/solicitudes/views.py
@@ -83,3 +83,39 @@ def crear_solicitud(request, id_proyecto, id_fase, id_item):
         
     template_name='./solicitudes/solicitudnueva.html'
     return render(request, template_name, {'form': form, 'id_proyecto':id_proyecto, 'id_fase': id_fase, 'id_item': id_item, 'proyecto':proyecto, 'fase':fase, 'item': item})
+
+def consultar_solicitud(request, id_proyecto, id_fase, id_solicitud):
+    """ Recibe un request, se verifica cual es el usuario registrado y el proyecto del cual se solicita,
+    se obtiene la lista de fases con las que estan relacionados el usuario y el proyecto 
+    desplegandola en pantalla, ademas permite realizar busquedas avanzadas sobre
+    las fases que puede mostrar.
+    
+    @type request: django.http.HttpRequest.
+    @param request: Contiene informacion sobre la solicitud web actual que llamo a esta vista.
+    
+    @rtype: django.shortcuts.render_to_response.
+    @return: fases.html, donde se listan las fases, ademas de las funcionalidades para cada fase.
+    
+    @author: Ysapy Ortiz.
+    
+    """
+    proyecto = Proyectos.objects.get(id=id_proyecto)
+    fase = Fases.objects.get(id=id_fase, proyecto_id=id_proyecto)
+    #item = Items.objects.get(proyecto_id=id_proyecto, fase_id=id_fase, id=id_item)
+    solicitud = Solicitudes.objects.get(id=id_solicitud)
+    # conseguir el contexto de las fases y sus estados
+    #fases = Fases.objects.filter(id_proyecto = id_proyecto)
+    template_name = './solicitudes/consultarsolicitud.html'
+    return render(request, template_name, {'id_proyecto': id_proyecto, 'fase': fase, 'id_fase': id_fase, 'proyecto':proyecto, 'fase':fase, 'solicitud': solicitud})
+
+def cancelar_solicitud(request, id_proyecto, id_fase, id_solicitud):
+    solicitud = Solicitudes.objects.get(id=id_solicitud)
+    solicitud.estado = 'Cancelado'
+    solicitud.save()
+    proyecto = Proyectos.objects.get(id=id_proyecto)
+    fase = Fases.objects.get(id=id_fase)
+    
+    mensaje = 'Solicitud eliminada con exito.'
+    template_name='./solicitudes/solicitudalerta.html'
+    ctx = {'mensaje': mensaje, 'id_proyecto':id_proyecto, 'id_fase': id_fase, 'proyecto':proyecto, 'fase':fase}
+    return render_to_response(template_name, ctx, context_instance=RequestContext(request))
