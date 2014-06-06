eefca00e1d86bcc5bf53a685bdcbe47536cc3050
ysapy <ysapy@Ysapy-Pc>
2014-06-06 12:34:50 -0400

    modificar comite sin solicitudes pendientes

--- sap/aplicaciones/comite/views.py
+++ sap/aplicaciones/comite/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from aplicaciones.roles.models import Roles
+from aplicaciones.solicitudes.models import Solicitudes
 
 # Create your views here.
     
