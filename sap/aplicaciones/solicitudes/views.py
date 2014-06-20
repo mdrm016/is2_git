04bcecf24d9aa9c4c19a8d6531973d94ffa826c9
ysapy <ysapy@Ysapy-Pc>
2014-06-20 18:02:00 -0400

    Eliminar datos de log

--- sap/aplicaciones/solicitudes/views.py
+++ sap/aplicaciones/solicitudes/views.py
@@ -567,3 +567,6 @@ def revertir_credencial(id_proyecto, id_fase, credencial):
                 relacionitem.save()
     item.version = version_anterior
     item.save()
+
+def probar():
+    noanda.save()
